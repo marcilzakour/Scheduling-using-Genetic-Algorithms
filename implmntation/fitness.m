c2863e91edc06280e1b7c6271595fd6866fc343c
marcilzakour <zakourmarsil@gmail.com>
2018-05-15 01:31:58 +0200

    Update fitness.m

--- implmntation/fitness.m
+++ implmntation/fitness.m
@@ -1,5 +1,8 @@
 function fx = fitness(x)
+
 BADFITNESS = inf;
+% the fitness is mainly clculated as (number of matchings - number of conflicts) - 5*repetions - n^-1 * c - n-2*a
+% where n is the total number of possible assignments
 n = length(x);
 c  = conflicts(x);
 r = repetitions(x);
